0d138ba5b71a6689d98cc5e50f0d538090183eb2
unknown <sujoydaspc2023@gmail.com>
2024-12-28 15:48:50 +0600

    Daily best sell component complete

--- src/Components/PopularProducts/PopularProducts.jsx
+++ src/Components/PopularProducts/PopularProducts.jsx
@@ -2,9 +2,9 @@ import PopularProduct from "./PopularProduct"
 
 const PopularProducts = () => {
   return (
-    <div className="max-w-screen-2xl lg:mx-auto mx-10 mt-10">
+    <div className="max-w-screen-2xl lg:mx-auto m-10">
       <h1 className="text-3xl font-semibold pb-10">Popular Products</h1>
-      
+
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
         <PopularProduct />
         <PopularProduct />
